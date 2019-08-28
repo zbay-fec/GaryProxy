ee820da5a509def7d28f7bff19d68db06b998e3d
Gary Blaser <glblaser@gmail.com>
2019-08-28 15:49:21 -0500

    Updated ec2 for bounced server

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
   deploy: {
     production: {
       user: "ubuntu",
-      host: "ec2-13-58-160-221.us-east-2.compute.amazonaws.com",
+      host: "ec2-18-219-137-130.us-east-2.compute.amazonaws.com",
       key: "~/.ssh/Zbay.pem",
       ref: "origin/master",
       repo: "https://github.com/zbay-fec/GaryProxy.git",
